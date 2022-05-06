1879712b01f321e8641c946e51e7182a95212a02
David Lenci <dlenci@scc-wl2.scc.bu.edu>
2022-05-06 10:13:32 -0400

    Finished correlation tab, just need to polish everything.

--- FinalProj/app.R
+++ FinalProj/app.R
@@ -6,6 +6,8 @@ library(tidyverse)
 library(colourpicker)
 library(DT)
 library(ComplexHeatmap)
+library(DESeq2)
+library(igraph)
 
 
 # Define UI for application that draws a histogram
@@ -82,7 +84,57 @@ ui <- fluidPage(
                                           )
                                       ),
                                       mainPanel(plotOutput("PCA"))
-                                  ))))))
+                                  )))))),
+        tabPanel("Differential Expression", fluid = TRUE,
+                 sidebarLayout(
+                     sidebarPanel(fileInput("de_file", "Input DE Results (csv)", accept = ".csv"),
+                                  hr(),
+                                  actionButton("runDE", "Perform DE Using Norm Counts"),
+                                  radioButtons(
+                                      "de_var",
+                                      "Select Variable for DE",
+                                      choices = "",
+                                      inline = T
+                                  )
+                                  
+                     ),
+                     mainPanel(
+                         sidebarLayout(
+                             sidebarPanel(
+                                 radioButtons("var_x", "X Variable", 
+                                              c("log2FoldChange", "stat", "lfcSE", "padj", "pvalue")),
+                                 radioButtons("var_y", "Y Variable",
+                                              c("padj", "pvalue", "log2FoldChange", "stat", "lfcSE")),
+                                 sliderInput("slider", min = -300, max = 0,
+                                             label ="significance", value = -100, step = 10),
+                                 colourInput("nonSig", "Select Color 1", value = "black",
+                                             showColour = "text"),
+                                 colourInput("sig", "Select Color 2", value = "red",
+                                             showColour = "text")
+                             ),
+                             mainPanel(
+                                 tabsetPanel(
+                                     tabPanel("Volcano Plot", plotOutput("volcano")),
+                                     tabPanel("Table", tableOutput("de_table"))
+                                 )))))),
+        tabPanel("Correlation Network Analysis",fluid = TRUE,
+                 sidebarLayout(
+                     sidebarPanel(fileInput("cor_file", "Input Norm Counts (csv)", 
+                                            accept = ".csv"),
+                                  textAreaInput("cor_genes", "Genes of Interest", 
+                                                width = "1000px"),
+                                  actionButton("runCor", 
+                                               "Perform Correlation Analysis"),
+                                  sliderInput("cor_slider", min = 0, max = 1,
+                                              label ="correlation", value = 0.5, step = 0.1)),
+                     mainPanel(
+                         tabsetPanel(
+                             tabPanel("Heatmap", plotOutput("cor_hm")),
+                             tabPanel("Correlation Graph", plotOutput("cor_graph")),
+                             tabPanel("Metrics", tableOutput("cor_metrics"))
+                         ))))
+        
+        
     )
 )
                         
@@ -109,6 +161,11 @@ server <- function(input, output, session) {
                 vchoices <- get_radio_names(data)
                 updateRadioButtons(session, "var", choices = vchoices)
             })
+            
+            observe({
+                dechoices <- get_de_names(data)
+                updateRadioButtons(session, "de_var", choices = dechoices)
+            })
             return(data)
         }
     })
@@ -129,13 +186,17 @@ server <- function(input, output, session) {
         return(list(type, value))
     }
     
-    summary_tb <- function(df){
-        types <- as.data.frame(lapply(df, function(x){helper(x)[[1]]}))
-        values <- as.data.frame(lapply(df, function(x){helper(x)[[2]]}))
+    summary_tb <- function(df_in){
+        df <- df_in
+        types <- lapply(df, function(x){helper(x)[[1]]})
+        values <- lapply(df, function(x){helper(x)[[2]]})
+        
+        types <- as.data.frame(types)
+        values <- as.data.frame(values)
         
         dataf <- as.data.frame(t(rbind(types, values)))
         dataf <- dataf %>%
-            filter(nchar(V2)<30)
+            filter(nchar(V2)<50)
         colnames(dataf) <- c("Type", "Mean (sd) or Distinct Values")
         dataf <- tibble::rownames_to_column(dataf, "Column Names")
         
@@ -291,6 +352,143 @@ server <- function(input, output, session) {
         return(plot)
     }
     
+    #DE
+    #-------------------------------------------------------------------------->
+    
+    load_de <- eventReactive(input$de_file, {
+        
+        if(!is.null(input$de_file$datapath)){
+            data <- read.csv(input$de_file$datapath,
+                             header = T)
+            rownames(data) <- data[,1]
+            data <- data[,-1]
+            return(data)
+        }else{
+            return(NULL)
+        }
+    })
+    
+    get_de_names <- function(dataf){
+        dataf <- dataf %>%
+            select_if(function(x) is.character(x))
+        names <- colnames(dataf)
+        return(names)
+    }
+    
+    run_deseq <- function(count_dataframe, meta, condition){
+        
+        samples <- colnames(count_dataframe)
+        design <- meta[,condition]
+        genes <- rownames(count_dataframe)
+        
+        coldata <- data.frame(samples=samples,
+                              diagnosis=design)
+        
+        count_dataframe <- as.data.frame(apply(count_dataframe, 2, as.integer))
+        #gen deseq object
+        dds <- DESeqDataSetFromMatrix(countData = count_dataframe,
+                                      colData = coldata,
+                                      design = ~ diagnosis)
+        
+        #perform de analysis
+        dds <- DESeq(dds)
+        res <- as.data.frame(results(dds))
+        rownames(res) <- genes
+        
+        return(res)
+    }
+    
+    
+    volcano_plot <-
+        function(dataf, x_name, y_name, slider, color1, color2) {
+            #clean data
+            dataf <- dataf %>%
+                drop_na(!!sym(x_name)) %>%
+                drop_na(!!sym(y_name))
+            #create plot
+            plot <- dataf %>%
+                ggplot(aes(!!sym(x_name), -log10(!!sym(y_name)), 
+                           colour = log10(!!sym(y_name)) < slider)) +
+                geom_point(stat="identity")+
+                scale_colour_manual(values=c(color1, color2)) +
+                xlab(x_name)+
+                ylab(y_name)+
+                theme_bw()
+            
+            return(plot)
+        }
+    
+    draw_table <- function(dataf, slider) {
+        #clean data
+        dataf <- dataf %>%
+            drop_na(padj) %>%
+            filter(log10(padj)<slider)%>%
+            arrange(padj)
+        
+        dataf$padj <- formatC(dataf$padj, digits = 5)
+        dataf$pvalue <- formatC(dataf$pvalue, digits = 5)
+        
+        return(dataf)
+    }
+    
+    #Correlation Plot
+    #-------------------------------------------------------------------------->
+    
+    load_cor <- eventReactive(input$cor_file,{
+        
+        if(!is.null(input$cor_file$datapath)){
+            data <- read.csv(input$cor_file$datapath,
+                             header = T)
+            rownames(data) <- data[,1]
+            data <- data[,-1]
+            return(data)
+        }else{
+            return(NULL)
+        }
+    })
+    
+    filter_cor <- function(data, genes){
+        genes <- strsplit(genes, "\n")
+        genes <- genes[[1]]
+        data <- data[genes,]
+        
+        return(data)
+    }
+    
+    heatmap_cor <- function(data, genes){
+        data_s <- filter_cor(data, genes)
+        plotData <- as.matrix(data_s)
+        
+        h <- Heatmap(plotData, name = 'Counts', cluster_rows = FALSE,
+                    column_gap = unit(0, "mm"), 
+                    show_row_names = FALSE, show_column_names = FALSE, 
+                    border = TRUE)
+        
+        return(h)
+    }
+    
+    corr_network <- function(data, thresh, genes){
+        data <- filter_cor(data, genes)
+        mat <- cor(t(data))
+        
+        mat[mat<thresh] <- 0
+        
+        # Make an Igraph object from this matrix:
+        network <- graph_from_adjacency_matrix( mat, weighted=T, 
+                                                mode="undirected", diag=F)
+        
+        # Basic chart
+        plot <- plot(network)
+        
+        stats <- data.frame(degree = degree(network),
+                            closeness = closeness(network),
+                            betweenness = betweenness(network))
+        
+        return(list(plot, stats))
+        
+    }
+    
+    
     
     #Output
     #-------------------------------------------------------------------------->
@@ -329,6 +527,54 @@ server <- function(input, output, session) {
                                   input$zero_slider,
                                   input$PC1,
                                   input$PC2)})
+    
+    #DE
+    
+    observeEvent(input$runDE,{
+        DESeq_result <- run_deseq(load_counts(), load_data(), input$de_var)
+        
+        output$volcano <- renderPlot({volcano_plot(DESeq_result, 
+                                                   input$var_x, 
+                                                   input$var_y,
+                                                   input$slider,
+                                                   input$nonSig,
+                                                   input$sig)
+        })
+        
+        output$de_table <- renderTable({draw_table(DESeq_result, input$slider)},
+                                    bordered = T) 
+        
+        
+    })
+    
+    output$volcano <- renderPlot({volcano_plot(load_de(), 
+                                               input$var_x, 
+                                               input$var_y,
+                                               input$slider,
+                                               input$nonSig,
+                                               input$sig)
+    })
+    
+    output$de_table <- renderTable({draw_table(load_de(), input$slider)},
+                                bordered = T) 
+    
+    #Correlation
+    
+    observeEvent(input$runCor, {
+        output$cor_hm <- renderPlot({heatmap_cor(load_cor(),
+                                                input$cor_genes)
+            })
+    
+        output$cor_graph <- renderPlot({corr_network(load_cor(),
+                                                    input$cor_slider,
+                                                    input$cor_genes)[1]})
+    
+        output$cor_metrics <- renderTable({corr_network(load_cor(),
+                                                        input$cor_slider,
+                                                        input$cor_genes)[2]}, 
+                                          bordered = T)
+    })
+    
 }
 
 # Run the application
